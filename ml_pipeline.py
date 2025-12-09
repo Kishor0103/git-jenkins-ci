c5278219719867c51094fe357636c5e87afb4a4d
Kishor <kishorsheldekar6@gmail.com>
2025-12-09 18:07:24 +0530

    ML-Pipeline

---
+++ ml_pipeline.py
@@ -0,0 +1,28 @@
+import pickle
+import sys
+import numpy as np
+from sklearn.linear_model import LinearRegression
+
+print("=== ML CI Pipeline ===")
+
+x= np.array([[1],[2],[3]])
+y= np.array([2,4,6])
+
+model = LinearRegression()
+model.fit(x,y)
+print("model trained")
+
+with open('model.pkl','wb') as f:
+    pickle.dump(model,f)
+print("model saved")
+
+prediction = model.predict([[4]])[0]
+print(f"Prediction: {prediction:.1f} (Expected: 8.0)")
+
+if abs(prediction - 8.0) < 0.1:
+    print("VALIDATION PASSED")
+    sys.exit(0)
+else:
+    print("VALIDATION PASSED")
+    sys.exit(1)
+    
